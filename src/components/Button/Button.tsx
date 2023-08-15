298646b35d0e61d34a630908df94fde71fc2bf8a
Alexandr Bochev <abochev@gmail.com>
2023-08-15 15:28:43 +0300

    channels title

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './Button.module.scss'
 
 interface IButton {
--- src/components/Сhannels/Сhannels.tsx
+++ src/components/Сhannels/Сhannels.tsx
@@ -1,4 +1,5 @@
 import styles from './Channels.module.scss'
+import lightning from '../../assets/svg/lightning.svg'
 import { logos } from '../../database/logos'
 import { channelsContent } from '../../database/content';
 import { useState } from 'react'
@@ -16,6 +17,13 @@ const Channels = () => {
   return (
     <section className='container'>
       <div className={styles.channels}>
+        <div className={styles.channels_title}>
+          <img src={lightning} alt="icon" className={styles.channels_icon} />
+          <h2>{channelsContent.title}</h2>
+        </div>
+        <p className={styles.channels_description}>{channelsContent.description}</p>
+      </div>
+      <div className={styles.channels_wrapper}>
         <div className={styles.channels_filter}>
           {channelsContent.categories.map(category =>
             <div onClick={() => handleCategoryChange(category)}>
