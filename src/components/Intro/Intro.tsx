b56e81676c2ad7ad9e7b9b00cd5c7144aa6b67f5
Alexandr Bochev <abochev@gmail.com>
2023-08-16 23:42:55 +0300

    motion

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,6 +3,7 @@ import styles from './Intro.module.scss'
 import IntroDevices from './IntroDevices/IntroDevices'
 import { introContent } from '../../database/content'
 import IntroAdvantage from './IntroAdvantage/IntroAdvantage'
+import { motion as m } from "framer-motion"
 
 const Intro = () => {
   return (
@@ -11,7 +12,15 @@ const Intro = () => {
           <div className={styles.intro}>
             <img className={styles.intro_background} src={introContent.background} alt="Background Top" />
             <div className={styles.intro_left}>
-              <h1 className={styles.intro_title}>{ introContent.title }</h1>
+              <m.h1
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.4, duration: 0.4, ease: "easeIn" }}
+                // transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
+                // whileInView={{ opacity: 1, y: 0 }}
+                className={styles.intro_title}>
+                  { introContent.title }
+              </m.h1>
               <div className={styles.intro_devices}><IntroDevices /></div>
               <p className={styles.intro_description}>{ introContent.description }</p>
               <Button name={introContent.button} red={true} />
