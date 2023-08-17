6968834fe9ddd3c96c0deac7c398947cb9bbdb55
Alexandr Bochev <abochev@gmail.com>
2023-08-17 14:39:35 +0300

    intro motion

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,6 +6,7 @@ import IntroAdvantage from './IntroAdvantage/IntroAdvantage'
 import { motion as m } from "framer-motion"
 
 const Intro = () => {
+  let num = 1
   return (
     <section className={styles.wrapper}>
       <div className='container'>
@@ -15,18 +16,39 @@ const Intro = () => {
               <m.h1
                 initial={{ opacity: 0, x: -100 }}
                 animate={{ opacity: 1, x: 0 }}
-                transition={{ delay: 0.4, duration: 0.4, ease: "easeIn" }}
+                transition={{ delay: 0.2, duration: 0.4, ease: "easeOut" }}
                 // transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
                 // whileInView={{ opacity: 1, y: 0 }}
                 className={styles.intro_title}>
                   { introContent.title }
               </m.h1>
               <div className={styles.intro_devices}><IntroDevices /></div>
-              <p className={styles.intro_description}>{ introContent.description }</p>
-              <Button name={introContent.button} red={true} />
+              <m.p
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
+                className={styles.intro_description}>
+                  { introContent.description }
+              </m.p>
+              <m.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.6, duration: 0.4, ease: "easeOut" }}
+              >
+                <Button name={introContent.button} red={true} />
+              </m.div>
             </div>
             <div className={styles.intro_advantages}>
-              { introContent.advantages.map((advantage) =><IntroAdvantage key={advantage.id} advantage={advantage} />) }
+              { introContent.advantages.map((advantage) =>
+                <m.div
+                  initial={{ opacity: 0, y: 100 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ delay: advantage.id * 0.2, duration: 0.4, ease: "easeOut" }}
+                  key={advantage.id}
+                >
+                  <IntroAdvantage advantage={advantage} />
+                </m.div>
+              ) }
             </div>
           </div>
         </div>
--- src/components/Intro/IntroDevices/IntroDevices.tsx
+++ src/components/Intro/IntroDevices/IntroDevices.tsx
@@ -5,17 +5,47 @@ import pumpkin from '../../../assets/webp/intro-pumpkin.webp'
 import macbook from '../../../assets/webp/intro-macbook.webp'
 import iphone from '../../../assets/webp/intro-iphone.webp'
 import ipad from '../../../assets/webp/intro-ipad.webp'
+import { motion as m } from "framer-motion"
 
 const IntroDevices = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.intro_devices}>
-        <img src={tv} alt="tv" className={styles.tv} />
-        <img src={shadow} alt="shadow" className={styles.shadow} />
-        <img src={pumpkin} alt="pumpkin" className={styles.pumpkin} />
-        <img src={macbook} alt="macbook" className={styles.macbook} />
-        <img src={iphone} alt="iphone" className={styles.iphone} />
-        <img src={ipad} alt="ipad" className={styles.ipad} />
+        <m.img
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0.4, duration: 0.4, ease: "easeOut" }}
+          src={tv} alt="tv" className={styles.tv} />
+
+        <m.img
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ delay: 1.4, duration: 0.4, ease: "easeIn" }}
+          src={shadow} alt="shadow" className={styles.shadow} />
+
+        <m.img
+          initial={{ opacity: 0, y: -400 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.4, duration: 0.4, ease: "easeIn" }}
+          src={pumpkin} alt="pumpkin" className={styles.pumpkin} />
+
+        <m.img
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.4, ease: "easeOut" }}
+          src={macbook} alt="macbook" className={styles.macbook} />
+
+        <m.img
+          initial={{ opacity: 0, x: -300 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.8, duration: 0.4, ease: "easeOut" }}
+          src={iphone} alt="iphone" className={styles.iphone} />
+
+        <m.img
+          initial={{ opacity: 0, y: 200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1, duration: 0.4, ease: "easeOut" }}
+          src={ipad} alt="ipad" className={styles.ipad} />
       </div>
     </div>
   )
